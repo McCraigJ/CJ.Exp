495eab1bfd61533d29f307330d4b68b7546eba42
Craig Jenkins <mccraigj@gmail.com>
2019-03-08 16:46:16 +0000

    Upgrade packages - WIP

---
+++ CJ.Exp.Auth.EFIdentity/EFAuthResultFactory.cs
@@ -0,0 +1,31 @@
+﻿using CJ.Exp.ServiceModels.Users;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+using CJ.Exp.ServiceModels;
+using Microsoft.AspNetCore.Identity;
+
+namespace CJ.Exp.Auth.EFIdentity
+{
+  public static class EFAuthResultFactory
+  {
+    public static AuthResultSM CreateResultFromIdentityResult(IdentityResult result)
+    {
+      return new AuthResultSM
+      {
+        Succeeded = result.Succeeded,
+        Errors = Mapper.Map<List<ProcessingErrorSM>>(result.Errors)
+      };
+    }
+
+    public static AuthResultSM CreateResultFromSignInResult(SignInResult result)
+    {
+      return new AuthResultSM
+      {
+        Succeeded = result.Succeeded,
+        IsLockedOut = result.IsLockedOut
+      };
+    }
+  }
+}
---
+++ MongoDb/js/addAdminUser.js
@@ -0,0 +1,6 @@
+var adminUser = 
+{
+  user: "admin",
+  pwd: "P@ssw0rd1",
+  roles: [ { role: "userAdminAnyDatabase", db: "admin" }, "readWriteAnyDatabase" ]
+};
