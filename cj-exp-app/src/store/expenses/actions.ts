d93991f4a917b84137395c23e62d8b9378825d08
Craig Jenkins <mccraigj@gmail.com>
2019-11-18 16:38:36 +0000

    Removed old front end react apps

--- cj-exp-app/src/store/expenses/actions.ts
+++
@@ -1,5 +0,0 @@
-import { action } from 'typesafe-actions';
-import { Expense, ExpenseActionTypes } from './types';
-
-export const addExpense = (data: Expense) => action(ExpenseActionTypes.ADD, data);
-export const editExpense = (data: Expense) => action(ExpenseActionTypes.EDIT, data);
